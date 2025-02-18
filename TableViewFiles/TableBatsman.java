dae43c10c0d40ec749a2683d3213878ca47d7a0e
onepieceIsReal <onepiece.iiith@gmail.com>
2025-02-18 15:07:33 +0530

    Refactor: Extract data classes for batsman and bowler statistics

--- TableViewFiles/TableBatsman.java
+++ TableViewFiles/TableBatsman.java
@@ -2,17 +2,41 @@ package TableViewFiles;
 
 public class TableBatsman {
 
-    private String playerName;
+    private BatsmanStats batsmanStats;
     private String outType;
+
+    public TableBatsman(BatsmanStats batsmanStats, String outType) {
+        this.batsmanStats = batsmanStats;
+        this.outType = outType;
+    }
+
+    public BatsmanStats getBatsmanStats() {
+        return batsmanStats;
+    }
+
+    public void setBatsmanStats(BatsmanStats batsmanStats) {
+        this.batsmanStats = batsmanStats;
+    }
+
+    public String getOutType() {
+        return outType;
+    }
+
+    public void setOutType(String outType) {
+        this.outType = outType;
+    }
+}
+
+class BatsmanStats{
+    private String playerName;
     private String batsmanRun;
     private String ballPlayed;
     private String fourRun;
     private String sixRun;
     private String strikeRate;
 
-    public TableBatsman(String playerName, String outType, String batsmanRun, String ballPlayed, String fourRun, String sixRun, String strikeRate) {
+    public BatsmanStats(String playerName, String batsmanRun, String ballPlayed, String fourRun, String sixRun, String strikeRate) {
         this.playerName = playerName;
-        this.outType = outType;
         this.batsmanRun = batsmanRun;
         this.ballPlayed = ballPlayed;
         this.fourRun = fourRun;
@@ -24,54 +48,46 @@ public class TableBatsman {
         return playerName;
     }
 
-    public String getOutType() {
-        return outType;
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
     }
 
     public String getBatsmanRun() {
         return batsmanRun;
     }
 
-    public String getBallPlayed() {
-        return ballPlayed;
-    }
-
-    public String getFourRun() {
-        return fourRun;
-    }
-
-    public String getSixRun() {
-        return sixRun;
-    }
-
-    public String getStrikeRate() {
-        return strikeRate;
-    }
-
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
-
-    public void setOutType(String outType) {
-        this.outType = outType;
-    }
-
     public void setBatsmanRun(String batsmanRun) {
         this.batsmanRun = batsmanRun;
+    }
+
+    public String getBallPlayed() {
+        return ballPlayed;
     }
 
     public void setBallPlayed(String ballPlayed) {
         this.ballPlayed = ballPlayed;
     }
 
+    public String getFourRun() {
+        return fourRun;
+    }
+
     public void setFourRun(String fourRun) {
         this.fourRun = fourRun;
+    }
+
+    public String getSixRun() {
+        return sixRun;
     }
 
     public void setSixRun(String sixRun) {
         this.sixRun = sixRun;
     }
- 
+
+    public String getStrikeRate() {
+        return strikeRate;
+    }
+
     public void setStrikeRate(String strikeRate) {
         this.strikeRate = strikeRate;
     }
